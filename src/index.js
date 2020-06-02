c8099b5d69a0a3d6899e540ce84245a778e95a2a
Lee Chase <leechase@live.com>
2020-06-02 11:26:15 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+      import React from 'react';
+      import { render } from 'react-dom';
+      import 'carbon-components/css/carbon-components.min.css';
+      import { Button } from 'carbon-components-react';
+    
+      const App = () => (
+        
+      <Button>Button</Button>
+    
+      );
+
+      render(<App />, document.getElementById('root'));
+    
